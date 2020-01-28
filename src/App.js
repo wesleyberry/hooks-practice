a9f94c1f30737fb00fd6326c2c28311a44c7e58c
Wesley Berry <wesleyberry52@gmail.com>
2020-01-28 14:14:08 -0600

    hooks with arrays

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
 import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <HookCounter />
       <HookCounterTwo />
       <HookCounterThree />
+      <HookCounterFour />
     </div>
   );
 }
