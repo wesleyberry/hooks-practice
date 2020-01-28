e58507e61e1ad6c42e69ebf573805bdb539410ff
Wesley Berry <wesleyberry52@gmail.com>
2020-01-28 13:32:37 -0600

    useState with objects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <ClassCounter />
       <HookCounter />
       <HookCounterTwo />
+      <HookCounterThree />
     </div>
   );
 }
