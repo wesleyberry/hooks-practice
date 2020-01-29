aa71fe50c1293d88bebcbc36061a38d8df9c909a
Wesley Berry <wesleyberry52@gmail.com>
2020-01-29 13:18:01 -0600

    fetching one item

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -3,26 +3,33 @@ import axios from 'axios';
 
 function DataFetching() {
 
-    const [posts, setPosts] = useState([]);
+    const [post, setPost] = useState({});
+    const [id, setId] = useState(1);
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(data => {
-                setPosts(data.data)
+                setPost(data.data)
             }).catch(err => console.log(err));
-    }, []);
+    }, [id]);
     
 
 
     return (
         <div>
-            <ul>
+            <input 
+                type="text" 
+                value={id}
+                onChange={e => setId(e.target.value)}
+            />
+            <div>{post.title}</div>
+            {/* <ul>
                 {posts.map(post => (
                     <li key={post.id}>
                         <h2>{post.title}</h2>
                     </li>
                 ))}
-            </ul>
+            </ul> */}
         </div>
     )
 }
